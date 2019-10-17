1af50b1c6995faa1128edffd99aa961fa088308c
dawidpytka <44122759+dawidpytka@users.noreply.github.com>
2019-10-17 10:21:53 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,192 @@
+"use strict"
+let todoList = []; //declares a new array for Your todo list
+let namesList = ["Title","Description","Place","Due Date",""];
+
+let initList = function() {
+    let savedList = window.localStorage.getItem("todos");
+    if (savedList != null)
+        todoList = JSON.parse(savedList);
+    else
+        todoList.push(
+        {
+            'title': "Learn JS",
+            'description': "Create a demo application for my TODO's",
+            'place': "445",
+            'dueDate': new Date(2019,10,16)
+        },
+        {
+            title: "Lecture test",
+            description: "Quick test from the first three lectures",
+            place: "F6",
+            dueDate: new Date(2019,10,17)
+        }
+            // of course the lecture test mentioned above will not take place
+    );
+}
+
+$.ajax({
+ // copy Your bin identifier here. It can be obtained in the dashboard
+ url: 'https://api.jsonbin.io/b/5da49fa29d04f724cee7dd46/latest',
+ type: 'GET',
+ headers: { //Required only if you are trying to access a private bin
+   'secret-key': "$2b$10$.ekc3fPIZbWgSufj4PKSGOhfaqj7uF.7RwLghfitcK6zWPWVoHPCC"
+ },
+ success: (data) => {
+   //console.log(data);
+todoList = data;
+ },
+ error: (err) => {
+   console.log(err.responseJSON);
+ }
+});
+
+let updateJSONbin = function() {
+    $.ajax({
+  url: 'https://api.jsonbin.io/b/5da49fa29d04f724cee7dd46',
+  type: 'PUT',
+  headers: { //Required only if you are trying to access a private bin
+    'secret-key': "$2b$10$.ekc3fPIZbWgSufj4PKSGOhfaqj7uF.7RwLghfitcK6zWPWVoHPCC"
+  },
+  contentType: 'application/json',
+  data: JSON.stringify(todoList),
+  success: (data) => {
+    console.log(data);
+  },
+  error: (err) => {
+    console.log(err.responseJSON);
+  }
+});}
+//initList();
+
+// let updateTodoList = function() {
+//     let todoListDiv =
+//     document.getElementById("todoListView");
+
+//     //remove all elements
+//     while (todoListDiv.firstChild) {
+//         todoListDiv.removeChild(todoListDiv.firstChild);
+//     }
+
+//     //add all elements
+//     let filterInput = document.getElementById("inputSearch");   
+//     for (let todo in todoList) {
+//       if (
+//         (filterInput.value == "") ||
+//         (todoList[todo].title.includes(filterInput.value)) ||
+//         (todoList[todo].description.includes(filterInput.value))
+//       ) {
+//         let newElement = document.createElement("p");
+//         let newContent = document.createTextNode(todoList[todo].title + " " +
+//                                                  todoList[todo].description);
+//         let newDeleteButton = document.createElement("input");
+//         newDeleteButton.type = "button";
+//         newDeleteButton.value = "x";
+//         newDeleteButton.addEventListener("click",
+//             function() {
+//                 deleteTodo(todo);
+//             });
+//         newElement.appendChild(newContent);
+//         todoListDiv.appendChild(newElement);
+//         newElement.appendChild(newDeleteButton);
+//       }
+//     }
+// }
+
+
+
+let updateTodoList = function() {
+
+    let table = document.querySelector("table");
+
+    //remove all elements
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+    }
+
+    let thead = table.createTHead();      
+    let row = thead.insertRow();
+    
+    for (let column in namesList){
+        let th = document.createElement("th");
+        let text = document.createTextNode(namesList[column]);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+    let att = document.createAttribute("class");
+    att.value = "thead-dark";
+    thead.setAttributeNode(att);
+    let tbody = table.createTBody();
+    let filterInput = document.getElementById("inputSearch");
+    let dateFilter1 = document.getElementById("inputSearchDate1");
+    let dateFilter2 = document.getElementById("inputSearchDate2"); 
+    for (let todo in todoList) {
+        if (
+            (filterInput.value == "") ||
+            (todoList[todo].title.includes(filterInput.value)) ||
+            (todoList[todo].description.includes(filterInput.value))
+        ){
+            if((dateFilter1.value == "" ||  dateFilter2.value == "" ) || (todoList[todo].dueDate > dateFilter1.value && todoList[todo].dueDate < dateFilter2.value)) {
+                let row = tbody.insertRow();
+                for(let key in todoList[todo]){
+                    let cell = row.insertCell();
+                    let text = document.createTextNode(todoList[todo][key]);
+                    cell.appendChild(text);
+                    att=document.createAttribute("class");
+                    att.value = "border";
+                    cell.setAttributeNode(att);
+                }
+                let newDeleteButton1 = document.createElement("input");
+                newDeleteButton1.type = "button";
+                newDeleteButton1.value = "x";
+                newDeleteButton1.addEventListener("click",
+                function() {
+                    deleteTodo(todo);
+                });
+                let cell = row.insertCell();
+                let text = document.createTextNode(newDeleteButton1.value);
+                cell.appendChild(newDeleteButton1);
+                att=document.createAttribute("class");
+                att.value = "border btn btn-outline-dark";
+                cell.setAttributeNode(att);
+            }
+        }
+    }
+}
+
+setInterval(updateTodoList, 1000);
+
+let deleteTodo = function(index) {
+    todoList.splice(index,1);
+    updateJSONbin();
+}
+
+let addTodo = function() {
+  //get the elements in the form
+    let inputTitle = document.getElementById("inputTitle");
+    let inputDescription = document.getElementById("inputDescription");
+    let inputPlace = document.getElementById("inputPlace");
+    let inputDate = document.getElementById("inputDate");
+  //get the values from the form
+    let newTitle = inputTitle.value;
+    let newDescription = inputDescription.value;
+    let newPlace = inputPlace.value;
+    let newDate = new Date(inputDate.value);
+  //create new item
+    let newTodo = {
+        title: newTitle,
+        description: newDescription,
+        place: newPlace,
+        dueDate: newDate
+    };
+  //add item to the list
+    todoList.push(newTodo);
+
+    window.localStorage.setItem("todos", JSON.stringify(todoList));
+    updateJSONbin();
+    
+    inputTitle.value="";
+    inputDescription.value="";
+    inputPlace.value="";
+    inputDate.value="";
+    
+}
